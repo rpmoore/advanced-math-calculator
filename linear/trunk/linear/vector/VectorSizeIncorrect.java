3da2aa766252c2b68391b694d3dd03239abc973e
Ryan <Ryan@ead10e89-aa9d-7727-f2b8-21115bd4e6d9>
2010-06-17 07:29:54 +0000

    Refactoring the code to be multi-threaded.

--- linear/trunk/linear/vector/VectorSizeIncorrect.java
+++ linear/trunk/linear/vector/VectorSizeIncorrect.java
@@ -1,6 +1,11 @@
 package linear.vector;
 
 public class VectorSizeIncorrect extends VectorException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2520632031582447028L;
+
 	public VectorSizeIncorrect()
 	{
 		super("The vectors are not the same size.");
---
+++ linear/trunk/linear/vectorService/AddService.java
@@ -0,0 +1,43 @@
+package linear.vectorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import linear.vector.Vector;
+import linear.vector.VectorSizeIncorrect;
+
+/**
+ * This service is to provide support for the adding of vectors concurrently.  A vector
+ * addition will be a blocking call as the service performs the addition until the service
+ * finishes the addition at which point the service will return a new vector with the result
+ * of the addition.  
+ * @author Ryan
+ *
+ */
+public class AddService {
+
+	private static final ExecutorService threadPool = Executors.newCachedThreadPool();
+	
+	/**
+	 * This performs a vector addition of vector a and vector b concurrently.  While the computation is
+	 * going on the add call is blocking, as soon as the computation is complete the call will return 
+	 * with a new vector containing the result.  If either of the parameters are null, then null is returned.
+	 * If the vectors do not have the same size an VectorSizeIncorrect is thrown stating so.
+	 * @param a The left argument for the addition.
+	 * @param b The right argument for the addition.
+	 * @return The result of the vector addition..
+	 */
+	public static Vector add(Vector a, Vector b) throws VectorSizeIncorrect
+	{
+		if(a == null || b == null)
+		{
+			return null;
+		}
+		if(a.size() != b.size())
+		{
+			throw new VectorSizeIncorrect();
+		}
+		
+		return null;
+	}
+}

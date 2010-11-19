a36a4b924157734f923f18cb8886ca0368c862ab
dogen.growkin <dogen.growkin@ead10e89-aa9d-7727-f2b8-21115bd4e6d9>
2010-11-19 08:55:34 +0000

    Starting work on rpn calculation.  Added apache 2 license information to all the source files as well.

--- trunk/bptree/ParseTree.java
+++ trunk/bptree/ParseTree.java
@@ -8,6 +8,23 @@ import DataStructures.BinaryTree;
 import DataStructures.TreeNode;
 import defIntegral.Calculate;
 
+/*
+ * Copyright 2010 Ryan Moore
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ */
+
 /**
  * Creates a Binary tree from an mathematical expression.  It then allows for the computations of values
  * based off of indexes passed in.  ie  The ParseTree represents a function f and you can insert a value x

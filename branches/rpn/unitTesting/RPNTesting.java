7394aad479999f62a39148b2cafa86afcfc771a9
dogen.growkin <dogen.growkin@ead10e89-aa9d-7727-f2b8-21115bd4e6d9>
2010-11-21 07:53:26 +0000

    Added test cases for some basic expressions for the RPN calculations.  Also started work on setting up the Stack to convert to RPN from Infix.

--- branches/rpn/unitTesting/RPNTesting.java
+++ branches/rpn/unitTesting/RPNTesting.java
@@ -28,6 +28,7 @@ public class RPNTesting extends TestCase {
 		try
 		{
 		
+			@SuppressWarnings("unused")
 			RPN rpnExpr = new RPN("x+x");
 			
 			assertTrue(true);
@@ -38,5 +39,84 @@ public class RPNTesting extends TestCase {
 			assertTrue(false);
 		}
 	
+	}
+	
+	public void testEval1()
+	{
+		try {
+			RPN rpnExpr = new RPN("2+3");
+			assertTrue(true);
+			assertEquals(5.0, rpnExpr.eval(0),0);//this should eval exactly.
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			assertTrue(false);
+		}
+		
+	}
+	
+	public void testEval2()
+	{
+		try
+		{
+			RPN rpnExpr = new RPN("x");
+			assertTrue(true);
+			assertEquals(3, rpnExpr.eval(3),0);
+		}
+		catch(ParseException e)
+		{
+			assertTrue(false);
+		}
+	}
+	public void testEval3()
+	{
+		try
+		{
+			RPN rpnExpr = new RPN("x+1");
+			assertTrue(true);
+			assertEquals(4, rpnExpr.eval(3),0);
+		}
+		catch(ParseException e)
+		{
+			assertTrue(false);
+		}
+	}
+	public void testEval4()
+	{
+		try
+		{
+			RPN rpnExpr = new RPN("x*2");
+			assertTrue(true);
+			assertEquals(6, rpnExpr.eval(3),0);
+		}
+		catch(ParseException e)
+		{
+			assertTrue(false);
+		}
+	}
+	public void testEval5()
+	{
+		try
+		{
+			RPN rpnExpr = new RPN("5-x");
+			assertTrue(true);
+			assertEquals(2, rpnExpr.eval(3),0);
+		}
+		catch(ParseException e)
+		{
+			assertTrue(false);
+		}
+	}
+	public void testEval6()
+	{
+		try
+		{
+			RPN rpnExpr = new RPN("x+2*5");
+			assertTrue(true);
+			assertEquals(12, rpnExpr.eval(2),0);
+		}
+		catch(ParseException e)
+		{
+			assertTrue(false);
+		}
 	}
 }

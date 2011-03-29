f843edbd0d62e70d1d48dd8db14f83ccdc35e732
dogen.growkin <dogen.growkin@gmail.com>
2011-03-29 18:06:52 +0000

    The Scientific Calculator is working again.  Need to work on the definite integral part now.

--- src/main/java/org/advancedMathCalculator/ui/integral/DefIntegralPanel.java
+++ src/main/java/org/advancedMathCalculator/ui/integral/DefIntegralPanel.java
@@ -5,9 +5,9 @@ import java.net.URL;
 import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
-import org.apache.pivot.wtk.Panel;
+import org.apache.pivot.wtk.BoxPane;
 
-public class DefIntegralPanel extends Panel implements Bindable{
+public class DefIntegralPanel extends BoxPane implements Bindable{
 
 	public void initialize(Map<String, Object> arg0, URL arg1, Resources arg2) {
 		System.out.println("Started DefIntegral Panel");

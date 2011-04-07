d140abdd514d7c323daf9ba826801eb99731f638
dogen.growkin@gmail.com <dogen.growkin@gmail.com@ead10e89-aa9d-7727-f2b8-21115bd4e6d9>
2011-04-07 09:08:55 +0000

    Removing all the old parser and lexer code.  Working on adding more test cases for the JavaCC generated parser.  Need to modify the parser code to handle parens, ie need to push left parens onto the operatorStack.  Then need to also work on doing implicit multiplication.  Currently some of the test cases do not work and will report both failures and errors.

--- trunk/src/test/java/org/advancedMathCalculator/EquationParserCCTest.java
+++ trunk/src/test/java/org/advancedMathCalculator/EquationParserCCTest.java
@@ -11,7 +11,6 @@ import org.advancedMathCalculator.computation.defIntegral.SimpsonsRule;
 import org.advancedMathCalculator.dataStructures.Queue;
 import org.advancedMathCalculator.parser.EquationToken;
 import org.advancedMathCalculator.parser.ExpressionType;
-import org.advancedMathCalculator.parser.RPN;
 import org.advancedMathCalculator.parser.cc.EquationParserCC;
 import org.advancedMathCalculator.parser.cc.ParseException;
 import org.advancedMathCalculator.parser.cc.RPNCC;
@@ -27,14 +26,14 @@ public class EquationParserCCTest extends TestCase {
 		Assert.assertEquals("2", queue.dequeue().getToken());
 		Assert.assertEquals("+", queue.dequeue().getToken());
 	}
-	
+
 	public void testBasicAddEval() throws ParseException, CalculateException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("3+2"));
 		RPNCC eval = new RPNCC(parser.parseEquation());
 		Assert.assertEquals(5.0, eval.eval(0),0.0);
 	}
-	
+
 	public void testBasicSub() throws ParseException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("5-12"));
@@ -43,14 +42,14 @@ public class EquationParserCCTest extends TestCase {
 		Assert.assertEquals("12",queue.dequeue().getToken());
 		Assert.assertEquals("-",queue.dequeue().getToken());
 	}
-	
+
 	public void testBasicSubEval() throws ParseException, CalculateException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("5-12"));
 		RPNCC eval = new RPNCC(parser.parseEquation());
 		Assert.assertEquals(-7.0,eval.eval(0),0.0);
 	}
-	
+
 	public void testBasicUnaryMinus() throws ParseException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("-2"));
@@ -58,14 +57,14 @@ public class EquationParserCCTest extends TestCase {
 		Assert.assertEquals("2", queue.dequeue().getToken());
 		Assert.assertEquals(ExpressionType.UNARYMINUS, queue.dequeue().getType());
 	}
-	
+
 	public void testBasicUnaryMinusEval() throws ParseException, CalculateException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("-2"));
 		RPNCC eval = new RPNCC(parser.parseEquation());
 		Assert.assertEquals(-2,eval.eval(0),0);
 	}
-	
+
 	public void testAddMult() throws ParseException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("5+12*2"));
@@ -76,7 +75,7 @@ public class EquationParserCCTest extends TestCase {
 		Assert.assertEquals("*",queue.dequeue().getToken());
 		Assert.assertEquals("+",queue.dequeue().getToken());
 	}
-	
+
 	public void testAddMultEval() throws ParseException, CalculateException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("5+12*2"));
@@ -84,6 +83,14 @@ public class EquationParserCCTest extends TestCase {
 		Assert.assertEquals(29, eval.eval(0),0);
 	}
 	
+	public void testParens() throws ParseException, CalculateException
+	{
+		EquationParserCC parser = new EquationParserCC(new StringReader("(5+2)*3"));
+		RPNCC eval = new RPNCC(parser.parseEquation());
+		Assert.assertEquals((5+2)*3, eval.eval(0),0);
+	}
+	
+
 	public void testTrigPow() throws ParseException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("2+3^sin(90)-4"));
@@ -97,28 +104,28 @@ public class EquationParserCCTest extends TestCase {
 		Assert.assertEquals("4", queue.dequeue().getToken());
 		Assert.assertEquals("-", queue.dequeue().getToken());
 	}
-	
+
 	public void testTrigPowEval() throws ParseException, CalculateException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("2+3^sin(90)-4"));
 		RPNCC eval = new RPNCC(parser.parseEquation());
 		Assert.assertEquals(.6702062608, eval.eval(0),.000000001);
 	}	
-	
+
 	public void testDefect2() throws ParseException, CalculateException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("log(1.5)^-1"));
 		RPNCC eval = new RPNCC(parser.parseEquation());
 		Assert.assertEquals(1/Math.log10(1.5), eval.eval(0),0);
 	}
-	
+
 	public void testCalcWithVar() throws ParseException, CalculateException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("2+x"));
 		RPNCC eval = new RPNCC(parser.parseEquation());
 		Assert.assertEquals(3, eval.eval(1),0);
 	}
-	
+
 	public void testCalcWithVarTwice() throws ParseException, CalculateException
 	{
 		EquationParserCC parser = new EquationParserCC(new StringReader("2-x"));
@@ -126,7 +133,7 @@ public class EquationParserCCTest extends TestCase {
 		Assert.assertEquals(1, eval.eval(1),0);
 		Assert.assertEquals(-1, eval.eval(3),0);
 	}
-	
+
 	public void testIntegrationSimpsionsRPN() throws CalculateException, ParseException {
 		Calculate expr = new RPNCC(new EquationParserCC(new StringReader(("12*x+.5"))).parseEquation());
 
@@ -134,4 +141,86 @@ public class EquationParserCCTest extends TestCase {
 		assertEquals(182.5, answer, 1);
 		assertEquals(182.5, answer, 0);
 	}
+
+	public void testLexLogEval() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("log(x)")).parseEquation());
+		assertEquals(Math.log10(5), function.eval(5),0);
+	}
+
+	public void testLexSinEval() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("sin(x)")).parseEquation());
+		assertEquals(Math.sin(5), function.eval(5),0);
+	}
+
+	public void testLexCosEval() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("cos(x)")).parseEquation());
+		assertEquals(Math.cos(5), function.eval(5),0);
+	}
+
+	public void testLexTanEval() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("tan(x)")).parseEquation());
+		assertEquals(Math.tan(5), function.eval(5),0);
+	}
+
+	public void testLexLNEval() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("ln(x)")).parseEquation());
+		assertEquals(Math.log(5), function.eval(5),0);
+	}
+
+	public void testEval8() throws ParseException, CalculateException
+	{
+		RPNCC	function = new RPNCC(new EquationParserCC( new StringReader("(12-8)^2/2")).parseEquation());
+		assertEquals(8, function.eval(0), 0);
+	}
+
+	public void testEval9() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("-2+3/x")).parseEquation());		
+		assertEquals(-1.0, function.eval(3),0);
+	}
+
+	public void testEval10() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("12x + 4")).parseEquation());
+		assertEquals(16.0, function.eval(1),0);
+	}
+
+	public void testEval11() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("3pix")).parseEquation());
+		assertEquals(3 * Math.PI * 2, function.eval(2),0);
+	}
+
+	public void testEval12() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("2+(-3+x)")).parseEquation());
+		assertEquals(2+(-3+1),function.eval(1),0);
+	}
+
+	public void testEval13() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("2+(-3x)")).parseEquation());
+		assertEquals(2+(-3*2),function.eval(2),0);
+	}
+	public void testEval14() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("2*-3x")).parseEquation());
+		assertEquals(2*-3*2, function.eval(2),0);
+	}
+	public void testEval15() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("2+(2*-3x)")).parseEquation());
+		assertEquals(2+(2*-3*2),function.eval(2),0);
+	}
+	public void testEval16() throws ParseException, CalculateException
+	{
+		RPNCC function = new RPNCC(new EquationParserCC(new StringReader("2+(2*(-3x))")).parseEquation());
+		assertEquals(2+(2*(-3*2)),function.eval(2),0);
+	}
+
 }

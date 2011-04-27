43bde6e47b7ddc51ed74eafff38d26aa0e65ca3f
dogen.growkin@gmail.com <dogen.growkin@gmail.com@ead10e89-aa9d-7727-f2b8-21115bd4e6d9>
2011-04-27 05:51:28 +0000

    Added the derivative calculation to the application.  Need to add the scientific calculator.

--- web/src/org/beginprogramming/web/client/Begin_Website.java
+++ web/src/org/beginprogramming/web/client/Begin_Website.java
@@ -1,32 +1,28 @@
 package org.beginprogramming.web.client;
 
+
+import org.beginprogramming.web.client.widgets.DerivativeWidget;
 import org.beginprogramming.web.client.widgets.IntegralWidget;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyDownEvent;
-import com.google.gwt.event.dom.client.KeyDownHandler;
-import com.google.gwt.event.dom.client.MouseDownEvent;
-import com.google.gwt.event.dom.client.MouseDownHandler;
-import com.google.gwt.user.client.Timer;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.dom.client.Style.Unit;
+
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.ScrollPanel;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.TabLayoutPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Begin_Website implements EntryPoint {
 	private final IntegralWidget integral = new IntegralWidget();
+	private final DerivativeWidget derivative = new DerivativeWidget();
 	public void onModuleLoad() {
-		RootPanel.get("integralComputation").add(integral);
+		final TabLayoutPanel tabPane = new TabLayoutPanel(1.5, Unit.EM);
+		tabPane.add(integral,"Integral");
+		tabPane.add(derivative,"Derivative");
+		RootPanel.get("integralComputation").add(tabPane);
+		tabPane.setSize("450px", "192px");
 		
 	}
 	
---
+++ web/src/org/beginprogramming/web/client/widgets/DerivativeWidget.java
@@ -0,0 +1,144 @@
+package org.beginprogramming.web.client.widgets;
+
+import org.beginprogramming.web.client.services.CalculationResponse;
+import org.beginprogramming.web.client.services.DerivativeService;
+import org.beginprogramming.web.client.services.DerivativeServiceAsync;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.DoubleBox;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PushButton;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class DerivativeWidget extends Composite {
+	private  DerivativeServiceAsync DerivativeService = GWT.create(DerivativeService.class);
+	private final PushButton sendButton;
+	private final TextBox equationText;
+	private final DoubleBox leftBounds;
+	private final HTML resultLabel;
+	private final DerivativeServiceProcess eventProcessing = new DerivativeServiceProcess();
+
+	public DerivativeWidget() {
+		
+		final VerticalPanel verticalPane = new VerticalPanel();
+		verticalPane.setStyleName("table#DerivativeTable");
+		final HorizontalPanel horizontalPanel = new HorizontalPanel();
+		horizontalPanel.setStyleName("DerivativeEquation");
+		sendButton= new PushButton("Compute Derivative");
+		equationText = new TextBox();
+		equationText.setText("Equation");
+		horizontalPanel.add(equationText);
+		horizontalPanel.add(sendButton);
+		verticalPane.add(horizontalPanel);
+		
+		HorizontalPanel horizontalPanel_1 = new HorizontalPanel();
+		horizontalPanel_1.setStyleName("DerivativeLowerBounds");
+		
+		InlineLabel nlnlblLowerBounds = new InlineLabel("Eval at x = ");
+		horizontalPanel_1.add(nlnlblLowerBounds);
+		
+		leftBounds = new DoubleBox();
+		horizontalPanel_1.add(leftBounds);
+		verticalPane.add(horizontalPanel_1);
+		
+		resultLabel = new HTML("Result Area", true);
+		
+		resultLabel.setStyleName("DerivativeTableResult");
+		verticalPane.add(resultLabel);
+		equationText.addKeyDownHandler(eventProcessing);
+		leftBounds.addKeyDownHandler(eventProcessing);
+		sendButton.addMouseDownHandler(eventProcessing);
+		initWidget(verticalPane);
+	}
+	
+	private class DerivativeServiceProcess implements MouseDownHandler, KeyDownHandler
+	{
+		final private DialogBox processing;
+		final Timer t = new Timer() 
+		{
+			public void run()
+			{
+				processing.center();
+			}
+		};
+		
+		public DerivativeServiceProcess()
+		{
+			processing = new DialogBox();
+			processing.setModal(true);
+			processing.setGlassEnabled(true);
+			processing.setTitle("Processing");
+			processing.setText("Processing");
+			processing.add(new Label("Processing Derivative Calculation..."));
+			
+		}
+		
+		@Override
+		public void onKeyDown(KeyDownEvent event) {
+			if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER && validate())
+			{
+				processRequest();				
+			}
+		}
+
+		@Override
+		public void onMouseDown(MouseDownEvent event) {
+			if(event.getSource() == sendButton && validate())
+			{
+				processRequest();
+			}
+		}
+		
+		private boolean validate()
+		{
+			String left = leftBounds.getText();
+			if(equationText.getText().isEmpty())
+			{
+				return false;
+			}
+			if(left.isEmpty())
+			{
+				return false;
+			}
+			return true;
+		}
+		
+		private void processRequest()
+		{
+			if(DerivativeService == null)
+			{
+				DerivativeService = GWT.create(DerivativeService.class);
+			}
+			t.schedule(750);
+			DerivativeService.derivativeService(equationText.getText(), leftBounds.getText(), new AsyncCallback<CalculationResponse>() {
+				@Override
+				public void onSuccess(CalculationResponse result) {
+					resultLabel.setText(result.getResultMessage());
+					t.cancel();
+					processing.hide();
+				}
+				@Override
+				public void onFailure(Throwable caught) {
+					t.cancel();
+					processing.hide();
+				}
+			});
+		}
+	}
+
+}

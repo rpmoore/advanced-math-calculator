b788fa16ea7d33e37b224f8d94f18d1e4b37e211
dogen.growkin@gmail.com <dogen.growkin@gmail.com@ead10e89-aa9d-7727-f2b8-21115bd4e6d9>
2011-06-16 17:34:22 +0000

    Updated some images and colors.

--- web/src/org/beginprogramming/web/client/widgets/MenuWidget.java
+++ web/src/org/beginprogramming/web/client/widgets/MenuWidget.java
@@ -3,7 +3,6 @@ package org.beginprogramming.web.client.widgets;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Image;
 
 public class MenuWidget extends Composite {
 
@@ -12,7 +11,7 @@ public class MenuWidget extends Composite {
 		//add all the hyperlinks that I will use.
 		panel.add(new HTML("<a style=\""+ style +"\" href=\"http://blog.begin-programming.com\">Blog</a>"));
 		panel.add(new HTML("<a style=\""+ style +"\"href=\"http://code.google.com/p/advanced-math-calculator/\">Google Code Site</a>"));		
-		panel.add(new Image("/images/IPv6-test-flight-blue-128-trans.png"));
+		panel.add(new HTML("<script src=\"http://www.stumbleupon.com/hostedbadge.php?s=2\"></script>"));
 		initWidget(panel);
 	}
 
